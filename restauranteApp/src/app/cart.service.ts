77377f5d94dc283b320d86bc68bf661cc59e214f
Jonatas Vidoretti Alves <jonatas101@hotmail.com>
2019-05-19 14:06:30 -0300

    inclusão das imgs

--- restauranteApp/src/app/cart.service.ts
+++ restauranteApp/src/app/cart.service.ts
@@ -8,46 +8,46 @@ export class CartService {
   private data = [
     {
       category: 'Pratos',
-      expanded: true,
+      expanded: false,
       products: [
-        {id: 0, name: 'nhoque', price: '29'},
-        {id: 1, name: 'spaghetti alla carbonara', price: '32'},
-        {id: 2, name: 'lasanha', price: '29'},
-        {id: 3, name: 'ravioli', price: '36'},
-        {id: 4, name: 'caneloni', price: '49'}
+        {id: 0, name: 'Nhoque', price: '29', img: '/assets/imgs/pratos/nhoque.jpg'},
+        {id: 1, name: 'Carbonara', price: '32', img: '/assets/imgs/pratos/carbonara.jpg'},
+        {id: 2, name: 'Lasanha', price: '29', img: '/assets/imgs/pratos/lasanha.jpg'},
+        {id: 3, name: 'Ravioli', price: '36', img: '/assets/imgs/pratos/ravioli.jpg'},
+        {id: 4, name: 'Caneloni', price: '49', img: '/assets/imgs/pratos/caneloni.jpg'}
       ]
     },
     {
       category: 'Lanches',
-      expanded: true,
+      expanded: false,
       products: [
-        {id: 0, name: 'chicken Parmesan', price: '17'},
-        {id: 1, name: 'Muffuletta', price: '31'},
-        {id: 2, name: 'Roasted Chicken Caprese', price: '29'},
-        {id: 3, name: 'Porchetta Sandwich', price: '27'},
-        {id: 4, name: 'Ciabatta Sandwich', price: '49'}
+        {id: 0, name: 'chicken Parmesan', price: '17', img: '/assets/imgs/lanches/caprese.jpg'},
+        {id: 1, name: 'Muffuletta', price: '31', img: '/assets/imgs/lanches/muffuletta.jpg'},
+        {id: 2, name: 'Roasted Chicken Caprese', price: '29', img: '/assets/imgs/lanches/chicken_caprese.jpg'},
+        {id: 3, name: 'Porchetta Sandwich', price: '27', img: '/assets/imgs/lanches/porchetta.jpg'},
+        {id: 4, name: 'Ciabatta Sandwich', price: '49', img: '/assets/imgs/lanches/ciabatta.jpg'}
       ]
     },
     {
       category: 'Bebidas',
-      expanded: true,
+      expanded: false,
       products: [
-        {id: 0, name: 'Refrigerante', price: '6'},
-        {id: 1, name: 'Cerveja', price: '10'},
-        {id: 2, name: 'Suco Natural', price: '6'},
-        {id: 3, name: 'Vinho', price: '59'},
-        {id: 4, name: 'Água', price: '3'}
+        {id: 0, name: 'Refrigerante', price: '6', img: '/assets/imgs/bebidas/refrigerantes.jpg'},
+        {id: 1, name: 'Cerveja', price: '10', img: '/assets/imgs/bebidas/cervejas.jpg'},
+        {id: 2, name: 'Suco Natural', price: '6', img: '/assets/imgs/bebidas/sucos.jpg'},
+        {id: 3, name: 'Vinho', price: '59', img: '/assets/imgs/bebidas/vinho.jpg'},
+        {id: 4, name: 'Água', price: '3', img: '/assets/imgs/bebidas/agua.png'}
       ]
     },
     {
       category: 'Sobremesa',
-      expanded: true,
+      expanded: false,
       products: [
-        {id: 0, name: 'Tiramisù', price: '29'},
-        {id: 1, name: 'Cassata Siciliana', price: '32'},
-        {id: 2, name: 'Panna Cotta', price: '29'},
-        {id: 3, name: 'Babà', price: '36'},
-        {id: 4, name: 'Tartufo di Pizzo', price: '49'}
+        {id: 0, name: 'Tiramisù', price: '29', img: '/assets/imgs/sobremesas/tiramisu.png'},
+        {id: 1, name: 'Cassata Siciliana', price: '32', img: '/assets/imgs/sobremesas/cassata.jpg'},
+        {id: 2, name: 'Panna Cotta', price: '29', img: '/assets/imgs/sobremesas/panna_cota.jpg'},
+        {id: 3, name: 'Babà', price: '36', img: '/assets/imgs/sobremesas/baba.jpg'},
+        {id: 4, name: 'Tartufo di Pizzo', price: '49', img: '/assets/imgs/sobremesas/tartufo.jpg'}
       ]
     }
   ];
